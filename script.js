9183bbfa648589e2bf8fac29c4d845f2828c9c6d
M1keCodingProjects <mikif2002@gmail.com>
2022-09-07 00:53:55 +0200

    code snippets are now automatically syntax-highlighted. Updated tutorial

--- script.js
+++ script.js
@@ -10,19 +10,54 @@ const preloadedModuleList = {
     //add here all the files you intend to load with the "use" procedure.
 };
 
-const printAction = () => {
-  console.clear();
-  console.log(textEditor.printText());
-}
-
 const usedFilePath = "EXAMPLES/presentation";
 
 const GLC = new Compiler(usedFilePath, preloadedModuleList);
 const textEditor = new TextEditor(await loadFile(usedFilePath));
 
 document.getElementById("run_button").onclick   = (() => GLC.run(textEditor.printText()));
-document.getElementById("print_button").onclick = printAction;
+document.getElementById("print_button").onclick = (() => {
+  console.clear();
+  console.log(textEditor.printText());
+});
+
 textEditor.begin_draw();
+
+console.log(textEditor.styleFile);
+const allSnippets    = Array.from(document.getElementsByClassName("codeSnippet"));
+const wordDelimiters = [" ", ".", "[", "\n"];
+
+const coerceChar = (char) => char == "\n" ? "<br>" : char;
+
+for(const snippet of allSnippets) {
+  const text = snippet.firstChild.innerHTML.replace(/<br>/g, "\n");
+  snippet.innerHTML = "";
+  let word = "";
+  let insideDelimiter = false;
+
+  for(const char of text.split("")) {
+    if(insideDelimiter) {
+      if(insideDelimiter.ends.includes(char)) {
+        snippet.innerHTML += `<font color="${insideDelimiter.color}">${word + coerceChar(char)}</font>`;
+        word = "";
+        insideDelimiter = false;
+        continue;
+      }
+    }
+    else {
+      if(char in textEditor.styleFile.delimiters) insideDelimiter = textEditor.styleFile.delimiters[char];
+      if(wordDelimiters.includes(char)) {
+        if(word in textEditor.styleFile.keywords) snippet.innerHTML += `<font color="${textEditor.styleFile.keywords[word]}">${word}</font>`;
+        else if(word.replace(/\s/g, "").length && !isNaN(word)) snippet.innerHTML += `<font color="${textEditor.styleFile.numbers}">${word}</font>`;
+        else snippet.innerHTML += word;
+        word = "";
+        snippet.innerHTML += coerceChar(char);
+        continue;
+      }
+    }
+    word += char;
+  }
+}
 
 /* TODO:
     Utmost priority:
